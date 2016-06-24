635fab9736981082f71f5024f40be5ec70bda3da
Robert Johnson <robert.m.johnson88@gmail.com>
2016-06-24 23:00:59 +0100

    Static framework data in angular

--- resources/public/angular/main.ctrl.js
+++ resources/public/angular/main.ctrl.js
@@ -1,4 +1,7 @@
 angular.module('app').controller("MainController", function() {
     var vm = this;
-    vm.title = 'AngularJS Tutorial Example';
+    vm.frameworks = [
+        { name: "Angular", stars: 43, forks: 15 },
+        { name: "React", stars: 34, forks: 9 }
+    ];
 });
